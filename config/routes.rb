8e03a69e063e16d208ec3a99cc044fc8f27f3c28
David M <davidmatamoros90@gmail.com>
2024-09-28 21:12:33 -0600

    adding devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
 end
